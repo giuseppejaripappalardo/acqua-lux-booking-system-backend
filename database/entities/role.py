4f013d38a97746ba5bc02b3b4828fdf63676c95e
Giuseppe Jari Pappalardo <giuseppejaripappalardo@gmail.com>
2025-03-31 00:23:02 +0200

    fix roles con enum

--- database/entities/role.py
+++ database/entities/role.py
@@ -1,17 +1,17 @@
 from datetime import datetime
-
-from sqlalchemy import String, DateTime, Integer
+from sqlalchemy import String, DateTime, Integer, Enum
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
 
 from database.entities.base import Base
 from utils.datetime_provider import DateTimeProvider
+from utils.enum.roles import Roles
 
 
 class Role(Base):
     __tablename__ = 'roles'
     id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True, autoincrement=True, nullable=False)
-    name: Mapped[str] = mapped_column(String(50), nullable=False, unique=True)
+    name: Mapped[Roles] = mapped_column( Enum(Roles, create_constraint=True), nullable=False, unique=True)
     description: Mapped[str] = mapped_column(String(255), nullable=True)
     created_at: Mapped[datetime] = mapped_column(DateTime, server_default=DateTimeProvider.get_timestamp_utc_sql(), nullable=False)
     modified_at: Mapped[datetime] =  mapped_column(DateTime, server_default=DateTimeProvider.get_timestamp_utc_sql(), onupdate=DateTimeProvider.get_timestamp_utc_sql(), nullable=False)
